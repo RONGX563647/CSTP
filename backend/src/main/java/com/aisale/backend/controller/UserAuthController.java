fe000f2c9c2de6613bc58386bb93dd242adca8b8
刘荣翔 <lrx563647@qq.com>
2026-04-12 14:12:47 +0800

    reflactor:更名auth为userauth

--- backend/src/main/java/com/aisale/backend/controller/UserAuthController.java
+++ backend/src/main/java/com/aisale/backend/controller/UserAuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-public class AuthController {
+public class UserAuthController {
 
     private final AuthService authService;
 
